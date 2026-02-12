b52587b10d13b660f91fce3927d90bace407814f
Sairam M <sairam.m.2005.65@gmail.com>
2026-02-12 17:01:48 +0530

    feat: Implement initial environment variable validation and tRPC client setup.

--- src/env.js
+++ src/env.js
@@ -25,6 +25,7 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_WS_URL: z.string().url(),
+    NEXT_PUBLIC_VERCEL_URL: z.string(),
   },
 
   /**
@@ -36,6 +37,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
